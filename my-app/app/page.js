752816c0ff5cf99c6c3c2fd8400941227b9ceb66
CherniIyes <cherniiyes@gmail.com>
2025-04-03 15:10:16 +0100

    poly

---
+++ my-app/app/page.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <div className="">
+      div
+
+
+
+      <Spline
+        scene="https://prod.spline.design/M6AJ-EEWvoHSvj5Q/scene.splinecode"
+      />
+
+
+
+    </div>
+  );
+}
